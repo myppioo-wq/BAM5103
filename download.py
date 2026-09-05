6a1888531ea2936b39065edf0949bb62704701ec
jihye <myppioo@g.skku.edu>
2026-09-05 14:58:49 +0900

    simple caculations and f-strings

--- download.py
+++ download.py
@@ -2,3 +2,12 @@ import requests
 
 r = requests.get("https://github.com/pimatskku/sturdy-memory/raw/refs/heads/main/dataset.zip")
 open("dataset.zip", "wb").write(r.content)
+
+
+import zipfile
+with zipfile.ZipFile("dataset.zip", "r") as zip_ref:
+    zip_ref.extractall("data")
+
+
+import os
+os.remove("dataset.zip")
